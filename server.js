aabedbf9f573ae8db999cbeba5f108c01d748c1a
Pranjal Kumar Bajaria <pranjal.bajaria@gmail.com>
2023-07-25 08:13:42 -0400

    first page setup

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express();
 const path = require('path');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, ''));
+app.set('views', path.join(__dirname, 'views'));
 
-app.listen(3000, () => {
-    console.log('Running on port 3000');;
+app.use('/assets', express.static('assets'));
+
+app.get("/", (req, res) => {
+    res.render('index', {});
+})
+
+app.listen(3001, () => {
+    console.log('Running on port 3001');;
 })
