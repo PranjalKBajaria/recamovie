fb2b74260fb77a67da6189c8d86baeaaa5db7fa9
Pranjal Kumar Bajaria <pranjal.bajaria@gmail.com>
2023-08-01 21:36:18 -0400

    API working

--- public/script.js
+++ public/script.js
@@ -1,25 +1,28 @@
-import { Configuration, OpenAIApi } from "openai";
+import openai from 'https://cdn.jsdelivr.net/npm/openai@3.3.0/+esm'
+
 // Running into bs issues with openai import
-const configuration = new Configuration({
+const configuration = new openai.Configuration({
   apiKey: "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",
 });
 
-const openai = new OpenAIApi(configuration);
+delete configuration.baseOptions.headers['User-Agent'];
 
-document.getElementById('findovie').onclick = async () => {
+const openAI = new openai.OpenAIApi(configuration);
+
+document.getElementById('findMovie').onclick = async () => {
     const prompt = document.querySelector('#movieInput').value;
-    const movies = queryGPT(prompt);
+    const movies = await queryGPT(prompt);
 }
 
 async function queryGPT(input) {
     const query = `Recommend me 3 movies based on the following prompt and return the answer in a JSON format \
     with their names (movieNames) in a list. "${input}"`;
   
-    const completion = await openai.createChatCompletion({
+    const completion = await openAI.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{ role: "assistant", content: query }],
     });
   
     const response = JSON.parse(completion.data.choices[0].message.content);
-    return response.movies;
+    return response.movieNames;
   }
